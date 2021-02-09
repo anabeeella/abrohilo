4ee9a3251f83fa8fd70ffa68c5f2f53f68d13005
Anabella Rizzi <anabeeella.r@gmail.com>
2021-02-09 16:21:31 -0300

    Nuevas páginas: quienes-somos, escribir-en-ah, temas-detalle

--- js/temas.js
+++ js/temas.js
@@ -23,20 +23,24 @@ let buildNotas = document.getElementById("grid-notas");
           notasTotal.forEach(allNotas => {
             buildNotas.innerHTML += 
                 `
-                <div class="col-4 mb-4">
-                    <div class="banner-autores d-flex justify-content-between px-3">
-                        <p class="header-cat-text text-uppercase">${allNotas.tag}</p>
+                <div class="mb-4 mx-2">
+                    <div class="banner-autores d-flex justify-content-between px-2">
+                        <div class="align-self-center">
+                          <img src="img/home/icono_contacto.png"><p class="header-cat-text text-uppercase d-inline">${allNotas.tag}</p>
+                        </div>
                         <p class="header-fecha">${allNotas.date}</p>
                     </div>
+                    <div>
                     <a href="${allNotas.link}" class="text-decoration-none">
                     <img class="w-100" alt="" src="https://via.placeholder.com/300x150">
-                    <div class=" bg-info p-4">
+                    <div class="bg-info p-4">
                         <p class="titular-news-home">${allNotas.title}</p>
                         <p class="bajada-news-home">${allNotas.description}</p>
                         <hr class="hr-news-home">
                         <p class="autor-news-home">Por ${allNotas.autor}</p>
                     </div>
                     </a>
+                    </div>
                 </div>
                 `
               }
